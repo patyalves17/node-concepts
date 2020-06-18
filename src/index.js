6494c2c90522611849c9ff1dadc92aa6c7216f14
Paty Alves <patriciaalves10@gmail.com>
2020-06-18 10:20:42 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
 
-const projects = [];
+const projects = [
+  {
+    id: '5b58b904-ac14-4104-8978-79190ab16001',
+    title: 'aplicativo React Nativa',
+    owner: 'paty',
+  },
+];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
